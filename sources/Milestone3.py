960ee32d435332b8a574223f411ddf7d7d73ed35
InesArauzo <ines.arauzo.andres@alumnos.upm.es>
2022-10-22 13:34:37 +0200

    note

--- sources/Milestone3.py
+++ sources/Milestone3.py
@@ -1,24 +1,39 @@
-from numpy import linspace, array
+from numpy import linspace, array, round
 import matplotlib.pyplot as plt
 from resources.Cauchy_Problem import Cauchy
 from resources.Time_Schemes import Euler, CN, RK4, BW_Euler
 from resources.Forcing_Sides import Kepler
-from resources.Error_Evaluation import Error_Cauchy_Problem
+from resources.Error_Evaluation import Error_Cauchy_Problem, Convergence_Rate
 
-N = 1000
-T = 10
-t = linspace(0, T, N)
+from matplotlib import rc # LaTeX tipography
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
+plt.rc('text', usetex=True); plt.rc('font', family='serif')
+
+import matplotlib
+matplotlib.rc('xtick', labelsize=18) 
+matplotlib.rc('ytick', labelsize=18)
+
+Nt = 200
+T = 40
+t = linspace(0, T, Nt)
 U0 = array ([1, 0, 0, 1])
 
 t_schemes = [ Euler, CN, RK4, BW_Euler ]
 scheme_order = [1, 2, 4, 1]
 t_scheme_name = [ "Euler", "CN" , "RK4", "BW_Euler" ]
 t_scheme_fig = [ "Euler", "CN" , "RK4", "BW_Euler" ]
+
+
 i = 0
 for t_scheme in t_schemes:
 
    
     E = Error_Cauchy_Problem(t_scheme, scheme_order[i], Kepler, U0, t)
+    
+
     plt.figure(figsize=(5, 5), layout='constrained')
     plt.plot(t, E[:, 0], label='X Error') 
     plt.plot(t, E[:, 1], label='Y Error')
@@ -31,6 +46,21 @@ for t_scheme in t_schemes:
     plt.title( t_scheme_name[i])  # Add a title to the axes.
     plt.legend()
     plt.show()
-    plt.savefig(t_scheme_fig[i])
-    i=i+1
+    plt.savefig('Error_' + t_scheme_fig[i] + '.png')
 
+    [log_diff_U21, log_Nt, lin_log_diff_U21, lin_log_Nt, order] = Convergence_Rate(t_scheme, Kepler, U0, t)
+    
+    
+    plt.plot(log_Nt, log_diff_U21, label = t_scheme_fig[i])
+    plt.plot(lin_log_Nt, lin_log_diff_U21, label = 'Linear regression')
+    plt.xlabel("log(Nt)")
+    plt.ylabel("log(U2-U1)")
+    plt.title(t_scheme_name[i] + " order = " +str(round(order)))
+    plt.legend()
+    plt.plot()
+    plt.grid()
+    
+    plt.savefig('Convergence_Rate' + t_scheme_name[i] + '.png')
+
+    i=i+1
+    
--- sources/resources/Error_Evaluation.py
+++ sources/resources/Error_Evaluation.py
@@ -1,8 +1,15 @@
-from numpy import linspace, zeros
+from numpy import linspace, zeros, log10, round_
 from numpy.linalg import norm
+from sklearn.linear_model import LinearRegression
 from resources.Cauchy_Problem import Cauchy
+#def Error_btw_grids (Time_Scheme, F, U0, t, a):
+
+#    t_2 =linspace(0, t[len(t) - 1], a*len(t))
+#    U_t = Cauchy(F, Time_Scheme, U0, t)
+#    U_t_2 =  Cauchy(F, Time_Scheme, U0, t_2)
 
 def Error_Cauchy_Problem (Time_Scheme, Scheme_Order, F, U0, t):
+
 
     # Computes the error of the obtained solution of a Cauchy problem
     # using the Richardson extrapolation:
@@ -31,28 +38,49 @@ def Convergence_Rate(Time_Scheme, F, U0, t):
     
     k = 10 # Numero de ptos que cojo en la gráfica (N1, N2=2*N1, N3=2*N2...) (numero de cauchy problems que te tienes que hacer)
     Nt = len(t)
+    T = t[Nt-1]
     NU = len(U0) 
+
     E = zeros([Nt, NU])
-    log_E = zeros(k)
-    log_N = zeros(k)
-    lin_log_E = zeros(k)
-    lin_log_N = zeros(k)
-    U_t = Cauchy(F, Time_Scheme, U0, t)
+    log_diff_U21 = zeros(k)
+    log_Nt = zeros(k)
+    lin_log_diff_U21 = zeros(k)
+    lin_log_Nt = zeros(k)
     order = 0
 
-    for i in range (k):
-        t_2 =linspace(0, t[Nt - 1], 2*Nt)
-        U_t_2 =  Cauchy(F, Time_Scheme, U0, t_2)
-        # Tiene que haber una forma de hacerlo llamando al Cauchy problem, pero hoy ya no puc mes
-
-
-
-
-
+    U_t = Cauchy(F, Time_Scheme, U0, t)
     
 
+    for i in range (k):
+
+        Nt = 2*Nt
+        t_2 =linspace(0, T, Nt)
+        U_t_2 =  Cauchy(F, Time_Scheme, U0, t_2)
+        #Deberias modificar el codigo del error pa 
+        # que puedas elegir la diferencia entre las mallas
+        # y para que te devuelva el error y la norma de 
+        # U2-U1, pero hoy ya no puc mes
+
+        log_diff_U21[i] = log10(norm(U_t_2[int(Nt - 1), :] - U_t[int(0.5*Nt - 1), :]))
+        log_Nt[i] = log10(Nt)
+
+        U_t = U_t_2
+        
+        for j in range(k):
+
+         if (abs(log_diff_U21[j]) > 12):
+
+             break
+        
+    l=min(j, k)
 
 
-    return [order, log_E, log_N, lin_log_E, lin_log_N ]
+    reg= LinearRegression().fit(log_Nt[0:j+1].reshape((-1, 1)),log_diff_U21[0:j+1]) 
+    order = round_(abs(reg.coef_),1)
+
+    lin_log_Nt = log_Nt[0:j+1]
+    lin_log_diff_U21 = reg.predict(log_Nt[0:j+1].reshape((-1, 1)))
+        
 
 
+    return [log_diff_U21, log_Nt, lin_log_diff_U21, lin_log_Nt, order]
